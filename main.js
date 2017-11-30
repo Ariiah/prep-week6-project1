19aebcb1780207a73db6ef3998d24cd4081d73df
Kelly L <32529793+Ariiah@users.noreply.github.com>
2017-11-30 10:36:56 -0700

    Add files via upload
    Edited JQuery inputs so all have document ready functions

--- main.js
+++ main.js
@@ -18,37 +18,45 @@ techniques may conflict with each other.
 
 
 // 1. Make sure that the HTML document is fully loaded before running any jQuery code that is needed below. Make sure you don't comment out this code as you'll wrap this code around all the following methods (so don't forget the closing tags, too.
-
+// $(document).ready(function(){
+//
+// });
 
 
 
 // 2. Hide all of the paragraphs on the index.html page.
-// $("p").hide();
+// $(document).ready(function() {
+//   // $("p").hide();
+// });
 
 
 
 
 // 3. Hide all of the h2's on the page.
+// $(document).ready(function() {
 // $("h2").hide();
-
+// });
 
 
 
 // 4. Hide all images on the page.
+// $(document).ready(function() {
 // $("img").hide();
-
+// });
 
 
 
 // 5. Hide all links on the page.
+// $(document).ready(function() {
 // $("a").hide();
-
+// });
 
 
 
 // 6. Hide all of the elements in the document.
+// $(document).ready(function() {
 // $("*").hide();
-
+// });
 
 
 // 7. Hide all paragraphs when one of them is clicked.
